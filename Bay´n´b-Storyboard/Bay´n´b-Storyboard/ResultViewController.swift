874aa8dc4026848489c2adfac2d3db4b0996d9a2
Sven Andabaka <sven.andabaka@tum.de>
2019-11-23 17:48:33 +0100

    result view results

--- Bay´n´b-Storyboard/Bay´n´b-Storyboard/ResultViewController.swift
+++ Bay´n´b-Storyboard/Bay´n´b-Storyboard/ResultViewController.swift
@@ -11,18 +11,41 @@ import MapKit
 
 class ResultViewController: UIViewController {
     
+    var addressFrom = ""
+    var addressTo = ""
+    var byCar = false
+    var byTrain = false
+    var byPlane = false
+    var car: Car?
+    var publicTransport: PublicTransport?
+    
     @IBOutlet weak var publicTransportView: UIView!
     @IBOutlet weak var publicTransportPrice: UILabel!
     @IBOutlet weak var publicTransportStartPoint: UILabel!
     @IBOutlet weak var publicTransportEndPoint: UILabel!
     @IBOutlet weak var publicTransportDepartureTime: UILabel!
     @IBOutlet weak var publicTransportArrivalTime: UILabel!
+    @IBOutlet weak var publicTransportLabel: UILabel!
+    @IBOutlet weak var noPublicTransportLabel: UILabel!
+    
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var startPointLabel: UILabel!
+    @IBOutlet weak var endPointLabel: UILabel!
+    @IBOutlet weak var departureTimeLabel: UILabel!
+    @IBOutlet weak var arrivalTimeLabel: UILabel!
     
     @IBOutlet weak var carView: UIView!
     @IBOutlet weak var carMap: MKMapView!
     @IBOutlet weak var carTime: UILabel!
     @IBOutlet weak var carDistance: UILabel!
     @IBOutlet weak var carPrice: UILabel!
+    
+    @IBOutlet weak var accommodationView: UIView!
+    @IBOutlet weak var accomodationRooms: UILabel!
+    @IBOutlet weak var accommodationBed: UILabel!
+    @IBOutlet weak var accommodationPrice: UILabel!
+    @IBOutlet weak var accommodationImage: UIImageView!
+    
     
     @objc func editTransport(_ gesture: UITapGestureRecognizer) {
         print("edit transport")
@@ -32,6 +55,92 @@ class ResultViewController: UIViewController {
         super.viewDidLoad()
         
         let recognizer = UITapGestureRecognizer(target: self, action: #selector(editTransport))
-        carView.addGestureRecognizer(recognizer)
+        
+        if byCar {
+            publicTransportView.isHidden = true
+            
+            carView.addGestureRecognizer(recognizer)
+            
+            car = Car(addressFrom: addressFrom, addressTo: addressTo)
+            car!.delegate = self
+        }
+        
+        if byTrain || byPlane {
+            carView.isHidden = true
+            
+            
+            publicTransportView.addGestureRecognizer(recognizer)
+            
+            if (byTrain) {
+                publicTransport = PublicTransport(cityStart: addressFrom, cityEnd: addressTo, byTrain: true, delegate: self)
+                trainValuesDidChange()
+            }
+            if (byPlane) {
+                publicTransport = PublicTransport(cityStart: addressFrom, cityEnd: addressTo, byTrain: false, delegate: self)
+                trainValuesDidChange()
+            }
+        }
+    
+//        accommodationImage.
+        
+
+        
+    }
+}
+extension ResultViewController: CarDelegate {
+    func carValuesDidChange() {
+        carMap.addOverlay((car!.route.polyline), level: MKOverlayLevel.aboveRoads)
+        
+        let rect = car!.route.polyline.boundingMapRect
+        self.carMap.setRegion(MKCoordinateRegion(rect), animated: true)
+        carTime.text = (car!.time.rounded(toPlaces: 0)/3600).rounded(toPlaces: 1).description + " h"
+        carDistance.text = car!.distance.rounded(toPlaces: 1).description + " km"
+        carPrice.text = car!.price.rounded(toPlaces: 2).description + " €"
+    }
+}
+
+extension ResultViewController: PublicTransportDelegate {
+    func trainValuesDidChange() {
+        if (publicTransport!.noPublicTransportAvailable) {
+            noPublicTransportLabel.isHidden = false
+            publicTransportArrivalTime.isHidden = true
+            publicTransportStartPoint.isHidden = true
+            publicTransportEndPoint.isHidden = true
+            publicTransportDepartureTime.isHidden = true
+            publicTransportPrice.isHidden = true
+            
+            priceLabel.isHidden = true
+            departureTimeLabel.isHidden = true
+            endPointLabel.isHidden = true
+            startPointLabel.isHidden = true
+            arrivalTimeLabel.isHidden = true
+            publicTransportLabel.isHidden = true
+
+            return
+        }
+        
+        publicTransportPrice.text = publicTransport!.publicTransportPrice.description
+        publicTransportEndPoint.text = publicTransport!.publicTransportEndPoint
+        publicTransportStartPoint.text = publicTransport!.publicTransportStartPoint
+        publicTransportArrivalTime.text = publicTransport!.publicTransportArrivalTime
+        publicTransportDepartureTime.text = publicTransport!.publicTransportDepartureTime
+    }
+}
+
+extension ResultViewController: MKMapViewDelegate {
+    func mapView(mapView: MKMapView, rendererForOverlay overlay: MKOverlay) -> MKOverlayRenderer {
+        let renderer = MKPolylineRenderer(overlay: overlay)
+        renderer.strokeColor = .red
+        renderer.lineWidth = 4.0
+    
+        return renderer
+    }
+}
+
+extension Double {
+    /// Rounds the double to decimal places value
+    func rounded(toPlaces places:Int) -> Double {
+        let divisor = pow(10.0, Double(places))
+        return (self * divisor).rounded() / divisor
     }
 }
